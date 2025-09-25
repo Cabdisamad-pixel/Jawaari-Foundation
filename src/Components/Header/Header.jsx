8800021d21eb9c349e3e70b2704d3663f9f22dad
Cabdisamad-pixel <cabdisamadmaxamed528@gmail.com>
2025-09-25 04:53:04 -0700

    updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,13 +4,15 @@ import Headerstyles from './Header.module.css';
 import { Navigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { createPortal } from 'react-dom';
-import { MdError } from "react-icons/md"; 
+import { MdError } from "react-icons/md";
+import { FaBars } from "react-icons/fa";   // Hamburger icon
+import { FaTimes } from "react-icons/fa";  // Close icon
 
 const SideNavBar = (props) => {
   return (
     <div className={Headerstyles.SideBarContainer}>
       <ul>
-        <i onClick={ () => props.responsiveHandler()} class="fa-solid fa-xmark"></i>
+        <i onClick={() => props.responsiveHandler()} class="fa-solid fa-xmark"></i>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About Us</Link></li>
         <li><Link to="/contact">Contact</Link></li>
@@ -41,8 +43,12 @@ const Header = () => {
           <li><Link to="/Blog">Blog</Link></li>
         </ul>
       </nav>
+      <button onClick={isMobileChangeHandler} className={Headerstyles.Menue}>
+        <FaBars style={{color:'#fff', height:'1rem'}} size={20} />
+      </button>
 
-      {isMobile && ReactDOM.createPortal(<SideNavBar responsiveHandler ={isMobileChangeHandler} />,document.getElementById('sideBarRoot'))}
+
+      {isMobile && ReactDOM.createPortal(<SideNavBar responsiveHandler={isMobileChangeHandler} />, document.getElementById('sideBarRoot'))}
 
     </header>
   );
